1dc32de289b4f5f968de40e87c4a5bd547184fe9
chromamorph <dave@titanmusic.com>
2020-03-13 13:02:58 +0100

    Updated static fields files

--- ooadp2009/src/dk/aau/imi/med4/ooadp2009/javainter/lesson01/Gizmo.java
+++ ooadp2009/src/dk/aau/imi/med4/ooadp2009/javainter/lesson01/Gizmo.java
@@ -7,42 +7,63 @@ package dk.aau.imi.med4.ooadp2009.javainter.lesson01;
 public class Gizmo {
 
 	/**
-	 * numG stores the number of Gizmos that have been constructed. It
+	 * numberOfGizmos stores the number of Gizmos that have been constructed. It
 	 * is static, which means that there is only one copy of this variable,
 	 * stored in the class itself. Each Gizmo object does not have its own copy
 	 * of this field.
 	 */
-	public static int numG = 0;
+	public static int numberOfGizmos = 0; //Note that name is in italics in Eclipse
 
 	/**
-	 * NOTnumG cannot also be the number of Gizmos because
-	 * it isn't static.
+	 * n is not declared as static, so there is a separate copy of n for each Gizmo
+	 * instance.
 	 */
-	public int NOTnumG = 0;
+	public int n = 0;
 
 	/**
-	 * id stores the id of this Gizmo
+	 * id stores the id of this Gizmo. It is non-static, so there is a separate copy
+	 * for each Gizmo instance.
 	 */
 	public int id = 0;
 
 	/**
-	 * NOTid can't be the ID because it is set using
-	 * NOTnumG which isn't static.
+	 * nid is another non-static variable that is updated using the value of n instead
+	 * of using the value of the static field, numberOfGizmos.
 	 */
-	public int NOTid = 0;
+	public int nid = 0;
 
 	public Gizmo() {
-		// Increase static field numberOfGizmos before setting id.
-		id = ++numG;
+		/*
+		 * We increment the value of the static field, numberOfGizmos,
+		 * and then assign the result to the non-static variable id.
+		 * 
+		 * Because numberOfGizmos is static, there is only one copy of it,
+		 * stored in the class itself (not any particular instance of the
+		 * class). Therefore each time a new Gizmo is instantiated using the 
+		 * Gizmo() no-args constructor, the static value of numberOfGizmos is
+		 * incremented and the Gizmo gets a unique value for its id variable.
+		 */
+		
+		id = ++numberOfGizmos;
 
-		// Following will always be set to 1.
-		NOTid = ++NOTnumG;
+		/*
+		 * We then increment n (which is not static) and assign 
+		 * the resulting value to nid.
+		 * 
+		 * Because n is initialized to 0 and there is a separate copy of
+		 * the variable for each instance, nid will always be set to 1
+		 * for each instance of Gizmo.
+		 */
+		
+		nid = ++n; 
 	}
 
 	public String toString() {
-		return "Gizmo: id=" + id + "; canIAlsoBeTheId=" + NOTid
-				+ "; canIAlsoBeTheNumberOfGizmos="
-				+ NOTnumG;
+		return "Gizmo:\n"
+				+ " numberOfGizmos = " + numberOfGizmos + "; "
+				+ " n = " + n + "; "
+				+ " id = " + id + "; "
+				+ " nid = " + nid + "\n";
 	}
 
 
--- ooadp2009/src/dk/aau/imi/med4/ooadp2009/javainter/lesson01/StaticFields.java
+++ ooadp2009/src/dk/aau/imi/med4/ooadp2009/javainter/lesson01/StaticFields.java
@@ -2,20 +2,41 @@ package dk.aau.imi.med4.ooadp2009.javainter.lesson01;
 
 public class StaticFields {
 	public static void main(String[] args) {
+		/*
+		 * When we access a static field, we can access it through
+		 * the class itself rather than through any specific instance
+		 * of the class. Indeed, we don't need to have instantiated 
+		 * the class at al in order to be able to print out the value
+		 * of one of its static fields.
+		 */
+
+		System.out.println("numberOfGizmos = " + Gizmo.numberOfGizmos);
+		
+		/*
+		 * Note that we cannot access a non-static field through the class
+		 * because there is a separate copy of such a field for each instance
+		 * of the class. So the following is not legal because n is not static.
+		 */
+		
+//		System.out.println("n = " + Gizmo.n);
+		
+		/* 
+		 * We need to first make an instance of the Gizmo class then access its
+		 * non-static variables through that instance, as follows:
+		 */
+		
+		Gizmo g = new Gizmo();
+		System.out.println("g.n = " + g.n);
+
+		/*
+		 * We'll now make 10 Gizmo objects and print them out.
+		 */
 		Gizmo[] gizmoArray = new Gizmo[10];
 		for (int i = 0; i < gizmoArray.length; i++)
 			gizmoArray[i] = new Gizmo();
 		for (Gizmo gizmo : gizmoArray)
 			System.out.println(gizmo);
-
-		System.out.println("numberOfGizmos = " + Gizmo.numG);
-
-		// Following won't work because canIAlsoBeTheNumberOfGizmos is not
-		// static and there is therefore a separate copy of this variable for
-		// each object constructed.
-
-		// System.out.println("canIAlsoBeTheNumberOfGizmos = " +
-		// Gizmo.canIAlsoBeTheNumberOfGizmos);
+		
 	}
 
 }
